9ecbc5ad8c707f856826760a46a22bebb8886f95
Nadiya Yeroshkina and Patrick Wells <patrick@>
2016-06-14 14:04:53 -0700

    add sponsor details and contact information

--- app/router.js
+++ app/router.js
@@ -9,6 +9,7 @@ Router.map(function() {
   this.route('results', {path: '/results/:zip'});
   this.route('committees-tile');
   this.route('bills-tile');
+  this.route('sponsor', {path: "/sponsor/:sponsor_id"});
 });
 
 export default Router;
